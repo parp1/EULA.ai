23b64bbd430fc8afd94985bf87080278e537da21
Ruban Rengaraju <32722516+rVrengaraju@users.noreply.github.com>
2021-03-07 01:40:32 -0800

    Added calculating screen (#77)
    Co-authored-by: Ruban Rengaraju <rubanrengaraju005@gmail.com>

--- frontend/src/components/Output/Output.jsx
+++ frontend/src/components/Output/Output.jsx
@@ -6,7 +6,7 @@ class Output extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			outputState: 0 /* 0 = idle, 1 = calculating, 2 = valid response, 3 = error */,
+			outputState: 1 /* 0 = idle, 1 = calculating, 2 = valid response, 3 = error */,
 			error: '',
 			classification: '',
 			summary: '',
@@ -19,9 +19,9 @@ class Output extends Component {
 
 	render() {
 		return (
-			<Container>
+			<Container state={this.state.outputState}>
 				<TopBar />
-				<BelowTopBar>
+				<BelowTopBar state={this.state.outputState}>
 					{this.state.outputState == 0 && (
 						<>
 							<IdleContainer>
@@ -32,7 +32,38 @@ class Output extends Component {
 					{this.state.outputState == 1 && (
 						<>
 							{/* TODO (Ruban): Calculating screen */}
-							<div>Calculating</div>
+							<YourEulaAnalysis>Calculating</YourEulaAnalysis>
+							<CalcHeaderWrapper> 
+								<CalcHeader>Hold On tight for<br></br>your result</CalcHeader>
+								<BigBar></BigBar>
+							</CalcHeaderWrapper>
+							<CalcTextContainer> 
+								<CalcHeaderSummary>Summary</CalcHeaderSummary>
+								<CalcBarGroup>
+									<CalcGreyBar width="50vh">.</CalcGreyBar>
+									<CalcGreyBar width="50vh">.</CalcGreyBar>
+									<CalcGreyBar width="37vh">.</CalcGreyBar>
+								</CalcBarGroup>
+								<CalcBarGroup>
+									<CalcGreyBar width="50vh">.</CalcGreyBar>
+									<CalcGreyBar width="37vh">.</CalcGreyBar>
+								</CalcBarGroup>
+								<CalcBarGroup>
+									<CalcGreyBar width="50vh">.</CalcGreyBar>
+									<CalcGreyBar width="50vh">.</CalcGreyBar>
+									<CalcGreyBar width="37vh">.</CalcGreyBar>
+								</CalcBarGroup>
+								<CalcBarGroup>
+									<CalcGreyBar width="50vh">.</CalcGreyBar>
+									<CalcGreyBar width="50vh">.</CalcGreyBar>
+									<CalcGreyBar width="37vh">.</CalcGreyBar>
+								</CalcBarGroup>
+							</CalcTextContainer>
+								
+							<CalcContainer>
+								<CalcImage Image src="calculating.png" />
+							</CalcContainer>
+							
 						</>
 					)}
 					{this.state.outputState == 2 && (
@@ -78,9 +109,94 @@ const IdleImage = styled.img`
 	width: auto;
 	margin-left: auto;
 	margin-right: auto;
+	z-index=-1;
 `;
 
 ////////////////////// TODO (Ruban): Calculating ///////////////////////
+
+const CalcContainer = styled.div`
+	height: 100%;
+	width: 100%;
+	text-align: center;
+	position: relative;
+	overflow: hidden;
+`;
+
+const CalcImage = styled.img`
+	height: 55vh;
+	margin-left: auto;
+	margin-right: auto;
+	left: 40px;
+	position: absolute;
+`;
+
+const CalcHeaderWrapper = styled.div`
+	display: grid;
+    grid-template-columns: 1fr 1fr;
+
+`;
+
+const BigBar = styled.div`
+	background: ${colors.BAR_GRAY};
+	height: 5vh;
+	width: 20vh;
+	margin: 0 auto;
+	margin-top: 2.5vh;
+	color: ${colors.BAR_GRAY};
+`;
+
+const CalcHeader = styled.div`
+	width: 100%;
+	font-weight: bolder;
+	font-size: 2.5vh;
+	letter-spacing: 1px;
+	color: ${colors.TEXT_GRAY};
+	text-align: left;
+	text-transform: uppercase;
+	margin-top: 2.5vh;
+`;
+
+const CalcHeaderSummary = styled.div`
+	width: 100%;
+	font-weight: bolder;
+	font-size: 2.5vh;
+	letter-spacing: 1px;
+	color: ${colors.TEXT_GRAY};
+	text-align: left;
+	text-transform: uppercase;
+	margin-top: 2.5vh;
+`;
+
+const CalcTextContainer = styled.div`
+	position: absolute;
+	overflow:hidden;
+`;
+
+const CalcGreyBar = styled.div`
+	margin-top: 1vh;
+	width: ${(props) => props.width};
+	height: 1.5vh;
+	background: ${colors.BAR_GRAY};
+	color: ${colors.BAR_GRAY};
+`;
+
+const CalcBarGroup = styled.div`
+	margin-top: 5vh;
+	padding-left: 20px;
+	padding-right: 20px;
+`;
+	
+
+/* Rectangle 497 */
+
+// position: absolute;
+// width: 348px;
+// height: 13px;
+// left: 873px;
+// top: 555px;
+
+// background: #D1D0D0;
+
 
 ////////////////////// Valid response ///////////////////////
 
@@ -185,6 +301,14 @@ const ErrorMessage = styled.div`
 
 ////////////////////// General ///////////////////////
 
+let setOverflow = (value) => {
+	if(value === 1) {
+		return "none"
+	} else {
+		return "scroll"
+	}
+}
+
 const BelowTopBar = styled.div`
 	margin-left: auto;
 	margin-right: auto;
@@ -193,7 +317,7 @@ const BelowTopBar = styled.div`
 	width: 96%;
 	padding: 0px 28px;
 	height: 69vh;
-	overflow-y: scroll;
+	overflow-y: ${(props) => setOverflow(props.state)};
 `;
 
 const TopBar = styled.div`
@@ -202,10 +326,18 @@ const TopBar = styled.div`
 	background-color: ${colors.DARK_PURPLE};
 `;
 
+let setColor = (value) => {
+	if (value === 1) {
+		return colors.LIGHTEST_GRAY
+	} else {
+		return colors.WHITE
+	}
+}
+
 const Container = styled.div`
 	height: 74vh;
 	width: 36vw;
-	background-color: ${colors.WHITE};
+	background-color: ${(props) => setColor(props.state)};
 `;
 
 export default Output;
